67eb363d40de516c0a32495c52ce5af6428e8788
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-22 18:32:53 -0300

    feat: implementando script que faz fastapi esperar o postegre inicializar

---
+++ scripts/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+until nc -z lu_estilo_db 5432; do
+    echo "Postegre iniciando..."
+    sleep 1
+done
+
+# alembic
+
+# Start the application
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
