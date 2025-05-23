89090d2940445498e0cc7ad8c462d913904702d0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-22 22:00:40 -0300

    feat: configurando database com sqlalchemy

---
+++ src/common/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from ..common.config import settings
+
+Base = declarative_base()
+
+engine = create_engine(settings.DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_db_engine():
+    engine.connect()
