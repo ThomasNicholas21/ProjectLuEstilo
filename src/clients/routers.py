76c6dee0447c83188c5776b40c0fcce9a08ec0a0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-23 20:57:25 -0300

    chore: ajustando identação

--- src/clients/routers.py
+++ src/clients/routers.py
@@ -12,51 +12,52 @@ client_router = APIRouter(prefix="/clients", tags=["Clients"])
 
 @client_router.post("/", response_model=ClientResponse)
 async def post_client(client: ClientCreate, db: Session = Depends(get_db)):
-        try:
-            db_client = Client(**client.__dict__)
-            db.add(db_client)
-            db.commit()
-            db.refresh(db_client)
-            return db_client
-        
-        except SQLAlchemyError as e:
-            db.rollback()
+    try:
+        db_client = Client(**client.__dict__)
+        db.add(db_client)
+        db.commit()
+        db.refresh(db_client)
+        return db_client
+    
+    except SQLAlchemyError as e:
+        db.rollback()
 
-            raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
+        raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
 
 
 @client_router.get("/", response_model=list[ClientResponse])
 async def get_client(db: Session = Depends(get_db)):
-        try:
-            response = db.query(Client).all()
-            return response
-    
-        except SQLAlchemyError as e:
-            db.rollback()
+    try:
+        response = db.query(Client).all()
+        return response
 
-            raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
+    except SQLAlchemyError as e:
+        db.rollback()
+
+        raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
 
 
 @client_router.get("/{id_client}", response_model=ClientResponse)
 async def get_detail_client(id_client: str , db: Session = Depends(get_db)):
-        try:
-            client = db.query(Client).get(id_client)
+    try:
+        client = db.query(Client).get(id_client)
 
-            if not client: 
-                    raise HTTPException(status_code=404, detail="Cliente não encontrado")
+        if not client: 
+                raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
-            return client
-        
-        except SQLAlchemyError as e:
-            db.rollback()
+        return client
+    
+    except SQLAlchemyError as e:
+        db.rollback()
 
-            raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
-        
+        raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
+    
 
 @client_router.put("/{id_client}", response_model=ClientResponse)
 async def put_detail_client(id_client: int, client_update: ClientUpdate, db: Session = Depends(get_db)):
     try:
         client = db.query(Client).get(id_client)
+
         if not client:
             raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
