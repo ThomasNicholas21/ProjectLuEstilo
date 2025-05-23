989526dfde9336eac2aebc0552297f6a051421a1
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-23 11:37:45 -0300

    refactor: melhorando nome de atributos e fazendo migração

--- src/orders/models.py
+++ src/orders/models.py
@@ -14,11 +14,11 @@ class Order(Base):
     __tablename__ = "order"
 
     id_order = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    client = Column(Integer, ForeignKey('client.id_client'), nullable=False)
+    id_client = Column(Integer, ForeignKey('client.id_client'), nullable=False)
     total_amount = Column(Integer, nullable=False)
     total_price = Column(Float, nullable=False)
     created_at = Column(DateTime, nullable=True)
     status = Column(Enum(OrderStatus), default=OrderStatus.PENDING)
     
-    client_rel = relationship("Client", back_populates="orders")
+    client = relationship("Client", back_populates="orders")
     items = relationship("OrderItem", back_populates="order")
