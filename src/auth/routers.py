f751a32a51ae2e7a5261e11ebc0313cecbf3cfc0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-24 14:05:08 -0300

    feat: implementando login

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from src.auth.security.token import get_password_hash
-from src.auth.serializer import UserRegister, UserResponse
+from src.auth.serializer import UserRegister, UserResponse, TokenResponse, UserLogin
 from src.auth.models import User
+from datetime import timedelta
 from src.common.database import get_db
+from src.auth.security.token import verify_password, create_access_token
 
 
 auth_router = APIRouter(prefix="/auth", tags=["User"])
@@ -27,3 +29,29 @@ def register(user_data: UserRegister, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@auth_router.post("/login", response_model=TokenResponse)
+async def login(user_data: UserLogin, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == user_data.username).first()
+    
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Usuário não encontrado."
+        )
+
+    if not verify_password(user_data.password, existing_user.password):
+        raise HTTPException(
+            status_code=401,
+            detail="Senha incorreta."
+        )
+
+    access_token = create_access_token(data={"sub": existing_user.username})
+    refresh_token = create_access_token(data={"sub": existing_user.username}, expires_delta=timedelta(days=7))
+
+    return TokenResponse(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        token_type="bearer"
+    )
