fc9e0581a82f351be4f1f4112c6a57b3949a006a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-24 19:27:56 -0300

    feat: criando mixin e model para Update

--- src/products/serializer.py
+++ src/products/serializer.py
@@ -3,8 +3,17 @@ from typing import Optional
 from datetime import datetime
 
 
-class ProductBase(BaseModel):
-    name: constr(min_length=1, max_length=100)  # type: ignore
+class EmptyStrToNoneMixin:
+    @field_validator('*', mode='before')
+    @classmethod
+    def empty_str_to_none(cls, value):
+        if value == "":
+            return None
+        return value
+
+
+class ProductBase(BaseModel, EmptyStrToNoneMixin):
+    name: constr(min_length=1, max_length=100) # type: ignore
     bar_code: str
     description: Optional[str] = None
     price: float
@@ -13,20 +22,25 @@ class ProductBase(BaseModel):
     category: Optional[str] = None
     section: Optional[str] = None
 
-    @field_validator('*')
-    def empty_str_to_none(cls, value):
-        if value == "":
-            return None
-        return value
+
+class ProductUpdate(BaseModel, EmptyStrToNoneMixin):
+    name: Optional[constr(min_length=1, max_length=100)] = None # type: ignore
+    bar_code: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    stock: Optional[int] = None
+    valid_date: Optional[datetime] = None
+    category: Optional[str] = None
+    section: Optional[str] = None
 
 
 class ProductCreate(ProductBase):
-    pass  
+    pass
 
 
 class ProductResponse(ProductBase):
     id_product: int
-    images: Optional[str] = None 
+    images: Optional[str] = None
 
     class Config:
         from_attributes = True
