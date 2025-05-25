424f84345a5da3a395d1154d5d8954fcdb410095
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-25 16:09:12 -0300

    feat: incluindo auxiliadores de registro de erro

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from src.common.config import settings
+from src.common.config import settings, init_sentry
 from src.common.database import create_db_engine
 from src.clients.routers import client_router
 from src.auth.routers import auth_router
 from src.products.routers import product_router
 from src.orders.routers import order_router
+from src.utils.exceptions import sentry_exception_middleware, register_exception_handlers
 
 
 @asynccontextmanager
@@ -20,6 +21,14 @@ app = FastAPI(
     version="1.0.0", 
     lifespan=lifespan
 )
+
+
+init_sentry()
+
+
+sentry_exception_middleware(app)
+register_exception_handlers(app)
+
 
 app.include_router(client_router)
 app.include_router(auth_router)
