311636f0fe18b827c1a27992adc8e61daf923338
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-25 12:23:56 -0300

    refactor: refatorando schemas

--- src/products/serializer.py
+++ src/products/serializer.py
@@ -7,40 +7,38 @@ class EmptyStrToNoneMixin:
     @field_validator('*', mode='before')
     @classmethod
     def empty_str_to_none(cls, value):
-        if value == "":
-            return None
-        return value
+        return None if value == "" else value
 
 
 class ProductBase(BaseModel, EmptyStrToNoneMixin):
-    name: constr(min_length=1, max_length=100) # type: ignore
-    bar_code: str
+    name: constr(min_length=1, max_length=100)  # type: ignore
+    bar_code: constr(min_length=1, max_length=50)  # type: ignore
     description: Optional[str] = None
     price: float
     stock: int
     valid_date: Optional[datetime] = None
-    category: Optional[str] = None
-    section: Optional[str] = None
-
-
-class ProductUpdate(BaseModel, EmptyStrToNoneMixin):
-    name: Optional[constr(min_length=1, max_length=100)] = None # type: ignore
-    bar_code: Optional[str] = None
-    description: Optional[str] = None
-    price: Optional[float] = None
-    stock: Optional[int] = None
-    valid_date: Optional[datetime] = None
-    category: Optional[str] = None
-    section: Optional[str] = None
+    images: Optional[str] = None
+    category: Optional[constr(max_length=50)] = None # type: ignore
+    section: Optional[constr(max_length=50)] = None # type: ignore
 
 
 class ProductCreate(ProductBase):
     pass
 
 
+class ProductUpdate(BaseModel, EmptyStrToNoneMixin):
+    name: Optional[constr(min_length=1, max_length=100)] = None  # type: ignore
+    bar_code: Optional[constr(min_length=1, max_length=50)] = None  # type: ignore
+    description: Optional[str] = None
+    price: Optional[float] = None
+    stock: Optional[int] = None
+    valid_date: Optional[datetime] = None
+    category: Optional[constr(max_length=50)] = None # type: ignore
+    section: Optional[constr(max_length=50)] = None # type: ignore
+
+
 class ProductResponse(ProductBase):
     id_product: int
-    images: Optional[str] = None
 
     class Config:
         from_attributes = True
