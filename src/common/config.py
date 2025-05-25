fdb39b682dba9b9b49b4b46b7926ab06ea5a390f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-25 16:09:41 -0300

    feat: incluindo sentry e nova variável de ambiente

--- src/common/config.py
+++ src/common/config.py
@@ -1,11 +1,27 @@
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Lu Estilo API"
     DATABASE_URL: str
     SECRET_KEY: str
+    SENTRY_DNS: str
 
     class Config:
         env_file = "dotenv/.env"
 
 settings = Settings()
+
+
+def init_sentry():
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DNS,
+        integrations=[
+            FastApiIntegration(),
+            SqlalchemyIntegration(),
+        ],
+        traces_sample_rate=1.0,  
+        environment="development"  
+    )
