29a0131119351cb2f11d9372d781e42ed2b0d921
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-25 02:46:30 -0300

    refactor: refatorando put_client e melhorando código (protegendo rota e validando role)

--- src/clients/routers.py
+++ src/clients/routers.py
@@ -85,40 +85,39 @@ async def get_client(
         )
 
 
-@client_router.get("/{id_client}", response_model=ClientResponse)
-async def get_detail_client(id_client: str , db: Session = Depends(get_db)):
-    try:
-        client = db.query(Client).get(id_client)
-
-        if not client: 
-                raise HTTPException(status_code=404, detail="Cliente não encontrado")
-
-        return client
+@client_router.put(
+    "/{id_client}",
+    response_model=ClientResponse,
+    summary="Atualizar cliente",
+    responses={404: {"description": "Cliente não encontrado"}}
+)
+async def put_detail_client(
+    id_client: int,
+    client_update: ClientUpdate,
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user)
+):
+    check_admin_permission(current_user)
     
-    except SQLAlchemyError as e:
-        db.rollback()
-
-        raise HTTPException(status_code=500, detail=f"Erro ao salvar no banco de dados: {e}")
+    client = db.query(Client).get(id_client)
+    if not client:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Cliente não encontrado"
+        )
     
-
-@client_router.put("/{id_client}", response_model=ClientResponse)
-async def put_detail_client(id_client: int, client_update: ClientUpdate, db: Session = Depends(get_db)):
     try:
-        client = db.query(Client).get(id_client)
-
-        if not client:
-            raise HTTPException(status_code=404, detail="Cliente não encontrado")
-
         update_data = client_update.model_dump(exclude_unset=True)
         for key, value in update_data.items():
             setattr(client, key, value)
-
         db.commit()
-        db.refresh(client)
         return client
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao atualizar cliente: {str(e)}")
+    
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Dados inválidos"
+        )
 
 
 @client_router.delete("/{id_client}", response_model=ClientResponse)
