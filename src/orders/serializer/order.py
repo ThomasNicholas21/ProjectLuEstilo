d0ca11e2c610f08fc3058431d60796a3c77d30e3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-24 21:11:06 -0300

    feat: criando serializer para order e orderitem

---
+++ src/orders/serializer/order.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+from enum import Enum
+from .orderitem import OrderItemCreate, OrderItemResponse
+
+
+class OrderStatusEnum(str, Enum):
+    pendente = "pendente"
+    pago = "pago"
+    cancelado = "cancelado"
+    em_andamento = "em_andamento"
+    finalizado = "finalizado"
+
+
+class OrderBase(BaseModel):
+    id_client: int
+    status: OrderStatusEnum
+
+
+class OrderCreate(OrderBase):
+    products: List[OrderItemCreate]
+
+
+class OrderUpdate(BaseModel):
+    status: Optional[OrderStatusEnum] = None
+
+
+class OrderResponse(BaseModel):
+    id_order: int
+    id_client: int
+    total_amount: int
+    total_price: float
+    status: OrderStatusEnum
+    created_at: datetime
+    items: List[OrderItemResponse]
+
+    class Config:
+        from_attributes = True
---
+++ src/orders/serializer/orderitem.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class OrderItemBase(BaseModel):
+    id_product: int
+    amount: int
+
+
+class OrderItemCreate(OrderItemBase):
+    pass
+
+
+class OrderItemResponse(OrderItemBase):
+    id_orderitem: int
+    unit_price: float
+
+
+    class Config:
+        from_attributes = True
